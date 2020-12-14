cc24f64066d74db1de7122ac29a7cd64e5809438
Charlie-Liao <75846514+Charlie-Liao@users.noreply.github.com>
2020-12-14 21:24:40 +0800

    背景初始化
    定義函數main，以進行初始化、載入圖片音樂等

---
+++ background.py
@@ -0,0 +1,12 @@
+import pygame as game
+
+def main():
+    game.init()
+    load images and sound
+    create sprites(visible objects) and groups for tracking them
+    
+    while keep playing:  # Game repeat cycle
+        while True:  # Game main loop
+            handle events
+            put new sprites on screen
+            check sprites collision 
